658d9b4bc2a8ddcc9cef614020a8eb4e66d6cc92
Suzi Nogueira <asuzinogueira@gmail.com>
2021-11-02 19:36:18 -0300

    exchange messages

---
+++ README.md
@@ -0,0 +1,1 @@
+### Chat - Utilizando Socket.IO
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const socketIo = require('socket.io')
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log('Running')
+})
+
+const messages = []
+
+const io = socketIo(server)
+
+io.on('connection', socket => {
+  console.log('New connection')
+  socket.emit('update_messages', messages)
+  socket.on('new_message', data => {
+    messages.push(data.msg)
+
+    io.emit('update_messages', messages)
+  })
 })
