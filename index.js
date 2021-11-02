1b68d24068c1fe0fc6eec8a2ec549c773a730ef2
Suzi Nogueira <asuzinogueira@gmail.com>
2021-11-02 19:48:04 -0300

    create users

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ io.on('connection', socket => {
   console.log('New connection')
   socket.emit('update_messages', messages)
   socket.on('new_message', data => {
-    messages.push(data.msg)
+    messages.push(data)
 
     io.emit('update_messages', messages)
   })
--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,7 @@
 const socket = io('http://localhost:3000/')
 
+let user = null
+
 socket.on('update_messages', messages => {
   updateMessagesOnScreen(messages)
 })
@@ -9,7 +11,7 @@ function updateMessagesOnScreen(messages) {
 
   let list_messages = '<ul>'
   messages.forEach(message => {
-    list_messages += `<li> ${message}</li>`
+    list_messages += `<li> ${message.user}: ${message.msg}</li>`
   })
   list_messages += `</ul>`
 
@@ -21,9 +23,20 @@ document.addEventListener('DOMContentLoaded', () => {
   form.addEventListener('submit', e => {
     e.preventDefault()
 
+    if (!user) {
+      alert('Defina um usuário')
+      return
+    }
     const message = document.forms['message_form_name']['msg'].value
     document.forms['message_form_name']['msg'].value = ''
-    socket.emit('new_message', { msg: message })
+    socket.emit('new_message', { user: user, msg: message })
     console.log(message)
+  })
+
+  const userForm = document.querySelector('#user_form')
+  userForm.addEventListener('submit', e => {
+    e.preventDefault()
+    user = document.forms['user_form_name']['user'].value
+    userForm.parentNode.removeChild(userForm)
   })
 })
